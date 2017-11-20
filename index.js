9594884959705798127c253822918f6238038ef8
Ronald DeBeras <rdeberas@gmail.com>
2017-11-20 02:19:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ function happyHolidaysTo(Ronald) {
 function happyHolidayTo(Christmas, Ronald) {
   return (`Happy ${Christmas}, ${Ronald}!`)
 }
+function holidayCountdown(5, Christmas) {
+  return (`It's ${5} days until ${Christmas}!`)
+}
